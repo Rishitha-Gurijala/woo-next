d4f37e1188fc18cc74b0d3741cd288efe1413775
Rishitha-Gurijala <thisisrishitha@gmail.com>
2024-09-19 02:50:50 +0530

    woocommerse

---
+++ wooConfig.js
@@ -0,0 +1,7 @@
+const wooConfig = {
+    siteUrl: 'http://localhost/wordpressonmac',
+    consumerKey: 'ck_a7286d041b4960ca40bde8acda67a492df928203',
+    consumerSecret: 'cs_33695a9cfc6e7c7d6d5b0a56316ee4e0e35285fa'
+}
+
+module.exports = wooConfig;
